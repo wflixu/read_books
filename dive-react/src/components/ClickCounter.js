1320c243efa53b8e119ec187e0a9a0d86d7c032a
wflixu <wflixu@yeah.net>
2018-11-11 14:47:20 +0800

    chap01

---
+++ dive-react/src/components/ClickCounter.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+class ClickCounter extends Component {
+    constructor(props){
+        super(props);
+        this.onClickButton = this.onClickButton.bind(this);
+        this.state = {
+            count:0
+        }
+    }
+    onClickButton(){
+        this.setState({
+            count:this.state.count+1
+        });
+    }
+
+    render(){
+        const counterStyle = {
+            margin: '16px'
+        }
+      return(
+          <div style={counterStyle}>
+              <button onClick={this.onClickButton}>click me</button>
+              <div>
+              click count: {this.state.count}
+              </div>
+          </div>
+      )
+    }
+}
+
+export default  ClickCounter;
