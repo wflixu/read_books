7e0854715ae3395a0d87514d52a95374d054e8af
wflixu <wflixu@yeah.net>
2018-11-25 11:51:25 +0800

    chap7-3

--- dive-react/src/App.js
+++ dive-react/src/App.js
@@ -1,47 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const cityCode = 101010100;
+import {view as CitySelector} from './city_selector/';
+import {view as Weather} from './weather/';
+
 
 class App extends Component {
-  constructor() {
-    super(...arguments);
-
-    this.state = {weather: null};
-  }
-
-  componentDidMount() {
-    const apiUrl = `/data/cityinfo/${cityCode}.html`;
-    fetch(apiUrl).then((response) => {
-      if (response.status !== 200) {
-        throw new Error('Fail to get response with status ' + response.status);
-      }
-      console.log('11111',response);
-      
-      response.json().then((responseJson) => {
-        console.log('222',responseJson);
-        
-        this.setState({weather: responseJson.weatherinfo});
-      }).catch((error) => {
-        this.setState({weather: null});
-      });
-    }).catch((error) => {
-      this.setState({weather: null});
-    });
-  }
-
+  
   render() {
-    if (!this.state.weather) {
-      return <div>暂无数据</div>;
-    }
-
-    const {city, weather, temp1, temp2} = this.state.weather;
-
     return (
       <div>
-        {city} {weather} 最低气温 {temp1} 最高气温 {temp2}
+        <CitySelector />
+        <Weather />
       </div>
-    )
+    );
   }
 }
 
