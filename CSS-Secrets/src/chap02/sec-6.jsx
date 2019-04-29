f476208f3463a93acc9a82975e2ac6a2c79111ca
wflixu <wflixu@yeah.net>
2019-04-29 23:09:13 +0800

    chap2

---
+++ CSS-Secrets/src/chap02/sec-6.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './sec-6.scss';
+
+export default class Ch02Se6 extends Component {
+    render() {
+        return (
+            <div>
+                <div class="box1 box"></div>
+                <div class="box2 box"></div>
+                <div class="box3 box"></div>
+
+                <h2>波点</h2>
+                <div class="box4 box"></div>
+                <div class="box5 box"></div>
+            </div>
+        );
+    }
+}
--- CSS-Secrets/src/main.js
+++ CSS-Secrets/src/main.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router';
 
 import Ch01Se1 from './chap01/se-1';
+import Ch02Se6 from './chap02/sec-6';
 
 class Home extends React.Component{
     render(){
         return (
             <div>
-                <Ch01Se1/>
+             
+                <Ch02Se6/>
             </div>
         ) ;
     }
